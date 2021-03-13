38d16227b883964b09a2cb0e567d52b5dfaad4c7
김승환 <dkadowlwh@naver.com>
2021-03-13 22:16:24 +0900

    Add: 시계 회전 시키기

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const circle_1 = document.getElementById('circle1');
 const circle_3 = document.getElementById('circle3');
+const hour = document.getElementById('hour-hand');
+const minute = document.getElementById('minute-hand');
+const second = document.getElementById('second-hand');
+
+const date = new Date();
 
 // 눈금 그리기
 for (let i = 0; i < 30; i++) {
@@ -30,3 +35,23 @@ for(let i = 0; i < 6; i++) {
   right++;
   left++;
 }
+
+// 시침, 분침, 초침 현재 위치 각도 계산
+const secDeg = (360 * (date.getSeconds() / 60)) + (6 * (date.getMilliseconds() / 1000));
+const minDeg = (360 * (date.getMinutes() / 60)) + (6 * (secDeg / 360));
+const hourDeg = (360 * (date.getHours() / 12)) + (30 * (minDeg / 360));
+
+// 시침, 분침, 초침 rotate 적용 함수
+function rotateAnimation(hand, duration, deg) {
+  hand.animate([
+    { transform: `rotate(${deg}deg)` },
+    { transform: `rotate(${deg + 360}deg)` }
+  ], {
+    duration: duration,
+    iterations: Infinity
+  });
+}
+
+rotateAnimation(hour, 43200000, hourDeg);
+rotateAnimation(minute, 3600000, minDeg);
+rotateAnimation(second, 60000, secDeg);
